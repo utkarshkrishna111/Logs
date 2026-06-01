76ca592a70ab4182ceb325e8adf64b9d405eed45
utkarshkrishna111 <53994305+utkarshkrishna111@users.noreply.github.com>
2026-06-01 17:01:59 +0000

    Create logsetup.ps1

---
+++ logsetup.ps1
@@ -0,0 +1,11 @@
+C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -File C:/Users/MrUtkarsh/PycharmProjects/email_compliance/setup.ps1
+At C:\Users\MrUtkarsh\PycharmProjects\email_compliance\setup.ps1:244 char:46
++ ... ite-Host "  Email data folder : email_data\" -ForegroundColor Magenta
++                                                ~~~~~~~~~~~~~~~~~~~~~~~~~~
+The string is missing the terminator: ".
+At C:\Users\MrUtkarsh\PycharmProjects\email_compliance\setup.ps1:67 char:8
++ } else {
++        ~
+Missing closing '}' in statement block or type definition.
+    + CategoryInfo          : ParserError: (:) [], ParentContainsErrorRecordException
+    + FullyQualifiedErrorId : TerminatorExpectedAtEndOfString
