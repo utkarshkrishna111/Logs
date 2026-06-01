735bb3517c25db10da3f0dca981af70a4ce4e876
utkarshkrishna111 <53994305+utkarshkrishna111@users.noreply.github.com>
2026-06-01 18:06:39 +0000

    Update logsetup.ps1

--- logsetup.ps1
+++ logsetup.ps1
@@ -15,3 +15,17 @@ Missing closing '}' in statement block or type definition.
 
  [ERR] Could not determine Python version. Raw output: ''
 
+
+ [1/7] Checking Python installation ...
+        Using Python executable: C:\Users\MrUtkarsh\AppData\Local\Microsoft\WindowsApps\python.exe
+You cannot call a method on a null-valued expression.
+At C:\Users\MrUtkarsh\PycharmProjects\email_compliance_check\setup (2).ps1:93 char:5
++     $pyVerRaw = (Get-Content $tmpVer -Raw -ErrorAction SilentlyContin ...
++     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : InvalidOperation: (:) [], RuntimeException
+    + FullyQualifiedErrorId : InvokeMethodOnNull
+ 
+        [ERR] Could not determine Python version from: C:\Users\MrUtkarsh\AppData\Local\Microsoft\WindowsApps\python.exe
+        [ERR] Try running manually:  & 'C:\Users\MrUtkarsh\AppData\Local\Microsoft\WindowsApps\python.exe' --version
+
+
